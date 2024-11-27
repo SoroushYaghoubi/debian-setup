a79aee9e25edc2ef6f327b0a3180a0add4252d59
Soroush Yaghoubi <soroush.yag@gmail.com>
2024-11-27 21:16:23 +0100

    removed clones

--- setup.sh
+++ setup.sh
@@ -9,8 +9,6 @@ sudo apt install -y gcc nasm make python3 pip nodejs npm
 # Install hacking tools
 sudo apt install -y gdb
 mkdir -p ~/repos
-sudo git clone https://github.com/pwndbg/pwndbg.git ~/repos/pwndbg && cd ~/repos/pwndbg && ./setup.sh
-sudo git clone https://github.com/ROPgadget/ROPgadget.git ~/repos/ROPgadget && cd ~/repos/ROPgadget && python3 setup.py install
 
 # Install network tools
 sudo apt install -y netcat wget telnet ssh
